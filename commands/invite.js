25d473cfbed5d4caf3c4696173505d3fb4680f08
progamerprop <74456772+progamerprop@users.noreply.github.com>
2020-11-17 23:52:57 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,12 +2,22 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.channel.send("https://discord.com/oauth2/authorize?client_id=777145151738085386&scope=bot&permissions=3147008");
+    var botEmbed = new discord.MessageEmbed()
+            .setTitle('Bot invite')
+            .setDescription("https://discord.com/oauth2/authorize?client_id=777145151738085386&scope=bot&permissions=3147008")
+            .setColor("#0099ff")
+            .addField("Bot naam", client.user.username)
+
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .setImage('https://i.imgur.com/wSTFkRM.png')
+            .setTimestamp()
+            .setFooter('Bot made by: Aron Gaming', 'https://i.imgur.com/wSTFkRM.png');
+
+        return message.channel.send(botEmbed);
+        console.log('Bot informatie is opgevraagd')
 
 }
 
 module.exports.help = {
-    name: "hallo",
-    description: "Geeft al de verschillende commands",
-    category: "Informatie"
+    name: "invite"
 }
